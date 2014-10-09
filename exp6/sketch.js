b5dfac76c3838cfa294a038160141a2bab3578f7
Derek Nassler <dnassler@rogers.com>
2014-10-09 15:17:49 -0400

    tweaked how color is changed and added not-in-use experimental SourceImage2 class

--- exp6/sketch.js
+++ exp6/sketch.js
@@ -1,3 +1,7 @@
+var globalSpeed = 1;
+var globalSpeedFactor = function() {
+  return globalSpeed;
+}
 
 window.onresize = function() {
   checkOrientation();
@@ -44,6 +48,8 @@ function setup() {
   angleMode(RADIANS);
 
   ImgMgr.instance = new ImgMgr();
+  //ImgMgr.instance.drawMode = 'lines';
+
 
 }
 
@@ -201,18 +207,38 @@ function SourceImage(sizeIn) {
   var nextAngle = random(-TWO_PI,TWO_PI);
   var nextTriangleAttrStart = millis();
   var nextTriangleAttrDur = 5000;
-
-  var fromX = random(size);
-  var fromY = random(size);
-  var toX = random(size);
-  var toY = random(size);
-  var x = fromX;
-  var y = fromY;
+  //var nextTriangleColorStart = millis();
+  //var nextTriangleColorDur = 5000;
 
   var minX = -size/2;
   var maxX = size*1.5;
   var minY = -size/2;
   var maxY = size*1.5;
+
+  var fromX = random(minX,maxX);//0
+  var fromY = random(minY,maxY);//0
+
+  var toX = random(minX,maxX);//size
+  var toY = random(minY,maxY);//size
+
+  var x = fromX;
+  var y = fromY;
+
+  console.log('size='+size+', fromX='+fromX+', fromY='+fromY+', toX='+toX+', toY='+toY);
+
+  var changeTriangleColor = function() {
+    //console.log('changeTriangleColor: before change: fromColor='+fromColor+', toColor='+toColor+', currentColorValueOnScale='+currentColorValueOnScale);
+
+    fromColor = toColor;
+    toColor = color(random(255),random(255),random(255),100);
+    currentColorValueOnScale = 0;
+
+    //console.log('changeTriangleColor: after change: fromColor='+fromColor+', toColor='+toColor+', currentColorValueOnScale='+currentColorValueOnScale);
+
+    //nextTriangleColorStart = millis();
+    //nextTriangleColorDur = 10000;
+
+  };
 
   var changeTriangleAttrs = function() {
 
@@ -221,33 +247,43 @@ function SourceImage(sizeIn) {
     toX = random(minX,maxX);
     toY = random(minY,maxY);
 
-    fromColor = toColor;
-    toColor = color(random(255),random(255),random(255),100);
-    currentColorValueOnScale = 0;
+    console.log('size='+size+', fromX='+fromX+', fromY='+fromY+', toX='+toX+', toY='+toY);
+
+    // fromColor = toColor;
+    // toColor = color(random(255),random(255),random(255),100);
+    // currentColorValueOnScale = 0;
 
     nextShapeSize = random(size/5,size*3);
+
+    fromAngle = angle;
     nextAngle = random(-PI,PI);
+
+    nextTriangleAttrStart = millis();
 
   };
 
   this.update = function() {
     
-    shapeSize += (nextShapeSize-shapeSize)/240;
+    shapeSize += (nextShapeSize-shapeSize)/(240*(1/globalSpeedFactor()));
 
-    if ( millis() <= nextTriangleAttrStart + nextTriangleAttrDur ){
-      angle = Math.easeInCubic( millis()-nextTriangleAttrStart, fromAngle, nextAngle, nextTriangleAttrDur );
-      x = Math.easeInOutCubic( millis()-nextTriangleAttrStart, fromX, toX-fromX, nextTriangleAttrDur);
-      y = Math.easeInOutCubic( millis()-nextTriangleAttrStart, fromY, toY-fromY, nextTriangleAttrDur);
+    if ( millis() <= nextTriangleAttrStart + nextTriangleAttrDur*(1/globalSpeedFactor()) ){
+      angle = Math.easeInCubic( millis()-nextTriangleAttrStart, fromAngle, nextAngle, nextTriangleAttrDur*(1/globalSpeedFactor()) );
+      x = Math.easeInOutCubic( millis()-nextTriangleAttrStart, fromX, toX-fromX, nextTriangleAttrDur*(1/globalSpeedFactor()));
+      y = Math.easeInOutCubic( millis()-nextTriangleAttrStart, fromY, toY-fromY, nextTriangleAttrDur*(1/globalSpeedFactor()));
     }
 
     currentColorValueOnScale += (1 - currentColorValueOnScale)/60;
     shapeColor = lerpColor(fromColor,toColor,currentColorValueOnScale);
     
-    if ( millis() > nextTriangleAttrStart + nextTriangleAttrDur ) {
+    // if ( millis() > nextTriangleColorStart + nextTriangleColorDur ) {
+    //   changeTriangleColor();
+    // }
+    //console.log('currentColorValueOnScale = '+currentColorValueOnScale);
+    if ( 1-currentColorValueOnScale < 0.01 ) {
+      changeTriangleColor();
+    }
+    if ( millis() > nextTriangleAttrStart + nextTriangleAttrDur*(1/globalSpeedFactor()) ) {
       changeTriangleAttrs();
-
-      nextTriangleAttrStart = millis();
-      fromAngle = angle;
     }
 
     drawGraphics();
@@ -281,6 +317,9 @@ function SourceImage(sizeIn) {
 
     if ( !ImgMgr.instance.drawMode || ImgMgr.instance.drawMode == 'triangle' ) {
       g.triangle(px0,py0, px1,py1, px2,py2);
+      //g.ellipse(px0,py0,10,10);
+      //g.ellipse(px1,py1,10,10);
+      //g.ellipse(px2,py2,10,10);
     } else {
       g.rect(px0,0,shapeSize,g.height*2);
       g.rect(px1,0,shapeSize,g.height*2);
@@ -291,6 +330,183 @@ function SourceImage(sizeIn) {
 
 }
 
+// ===
+
+function SourceImage2(sizeIn) {
+
+  var size = sizeIn;
+  var g = createGraphics(size,size);
+  this.getImage = function() {
+    return g;
+  }
+
+  var rotate = function(angle,x,y) {
+    return {x:x*cos(angle)-y*sin(angle),y:x*sin(angle)+y*cos(angle)};
+  };
+
+  var fromColor = color(random(255),random(255),random(255), 255);
+  var toColor = color(random(255),random(255),random(255), 255);
+
+  var shapeColor;
+  var currentColorValueOnScale = 0;
+
+  var shapeSize = random(10,size*0.5);
+  var nextShapeSize = random(10,size*0.5);
+
+  var angle = PI;
+  var fromAngle = angle;
+  var nextAngle = random(-TWO_PI,TWO_PI);
+  var nextTriangleAttrStart = millis();
+  var nextTriangleAttrDur = 5000;
+
+  var fromX = random(size);
+  var fromY = random(size);
+  var toX = random(size);
+  var toY = random(size);
+  var x = fromX;
+  var y = fromY;
+
+  var minX = -size/2;
+  var maxX = size*1.5;
+  var minY = -size/2;
+  var maxY = size*1.5;
+
+  var lightLength = random(size/2,size*2);
+  var lightHeight = random(size/10,size/5);
+  var lightColor1;
+  var lightColor2;
+  var lightColorX;
+  var lightColor;
+  var lightX;
+  var lightY;
+  var lightXvel = 10;
+  var pillarX;
+  var pillarLength = size*2;
+  var pillarDist = random(size,size*3);
+
+  var changeTriangleColor = function() {
+    fromColor = toColor;
+    toColor = color(random(255),random(255),random(255),100);
+    currentColorValueOnScale = 0;
+  };
+  var changeTriangleAttrs = function() {
+
+    fromX = toX;
+    fromY = toY;
+    toX = random(minX,maxX);
+    toY = random(minY,maxY);
+
+    changeTriangleColor();
+
+    nextShapeSize = random(size/5,size*3);
+
+    fromAngle = angle;
+    nextAngle = random(-PI,PI);
+
+    lightLength = random(size/2,size*2);
+    lightHeight = random(size/10,size/2);
+    lightColor1 = color(255,100,0);
+    lightColor2 = color(100,255,100);
+    lightColorX = random(1);
+    lightColor = lerpColor(lightColor1,lightColor2,lightColorX);
+
+    pillarX = lightX + random(size,size*2);
+    pillarLength = random(size/5,size*2);
+    pillarDist = random(size,size*3);
+
+    lightX = size*2;
+    lightDistX = random(size*2, size*10);
+    lightY = random(size);
+
+    nextTriangleAttrStart = millis();
+
+  };
+
+  this.update = function() {
+    
+    shapeSize += (nextShapeSize-shapeSize)/240;
+
+    if ( millis() <= nextTriangleAttrStart + nextTriangleAttrDur ){
+      angle = Math.easeInCubic( millis()-nextTriangleAttrStart, fromAngle, nextAngle, nextTriangleAttrDur );
+      x = Math.easeInOutCubic( millis()-nextTriangleAttrStart, fromX, toX-fromX, nextTriangleAttrDur);
+      y = Math.easeInOutCubic( millis()-nextTriangleAttrStart, fromY, toY-fromY, nextTriangleAttrDur);
+    }
+    lightX -= lightXvel;
+    if ( lightX < -100 ) {
+      lightX = size + lightDistX;
+    }
+    pillarX -= lightXvel;
+    if ( pillarX < -100 ) {
+      pillarX = size + pillarDist;
+    }
+
+    currentColorValueOnScale += (1 - currentColorValueOnScale)/60;
+    shapeColor = lerpColor(fromColor,toColor,currentColorValueOnScale);
+    
+    if ( 1-currentColorValueOnScale < 0.01 ) {
+      changeTriangleColor();
+    }
+
+    if ( millis() > nextTriangleAttrStart + nextTriangleAttrDur ) {
+      changeTriangleAttrs();
+
+    }
+
+    drawGraphics();
+  };
+
+  var drawGraphics = function() {
+    g.background(0);
+    //g.clear();
+    g.rectMode(CENTER);
+    g.ellipseMode(CENTER);
+    //g.fill(shapeColor);
+    g.fill(lightColor);
+    g.noStroke();
+    //g.rect(x,y,shapeSize,shapeSize);
+    //g.ellipse(x,y,shapeSize,shapeSize);
+
+    // var sx0 = 0;
+    // var sy0 = -shapeSize/2;
+    // var sx1 = shapeSize/2;
+    // var sy1 = shapeSize/2;
+    // var sx2 = -shapeSize/2;
+    // var sy2 = shapeSize/2;
+
+    // var r = rotate(angle,sx0,sy0);
+    // var px0 = r.x + x;
+    // var py0 = r.y + y;
+    // r = rotate(angle,sx1,sy1);
+    // var px1 = r.x + x;
+    // var py1 = r.y + y;
+    // r = rotate(angle,sx2,sy2);
+    // var px2 = r.x + x;
+    // var py2 = r.y + y;
+
+    g.fill(255,50);
+    g.rect(pillarX,0,pillarLength,size*2);
+
+    g.fill(lightColor);
+    g.rect(lightX,lightY,lightLength,lightHeight);
+
+    // if ( !ImgMgr.instance.drawMode || ImgMgr.instance.drawMode == 'triangle' ) {
+    //   g.triangle(px0,py0, px1,py1, px2,py2);
+      
+    // } else {
+    //   g.rect(px0,0,shapeSize,g.height*2);
+    //   g.rect(px1,0,shapeSize,g.height*2);
+    //   g.rect(px2,0,shapeSize,g.height*2);
+    // }
+
+  };
+
+}
+
+Math.easeOutCubic = function (t, b, c, d) {
+  t /= d;
+  t--;
+  return c*(t*t*t + 1) + b;
+};
 
 Math.easeInOutCubic = function (t, b, c, d) {
   t /= d/2;
